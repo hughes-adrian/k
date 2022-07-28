c0fd693fa7b1965de34e235ecd647229ea5b5f61
hughes-adrian <hughesadrian@gmail.com>
2022-07-28 22:35:55 +0200

    create Ast visitors

---
+++ src/AstCodeGen.java
@@ -0,0 +1,127 @@
+import java.lang.reflect.GenericDeclaration;
+import java.util.List;
+
+public class AstCodeGen implements Exp.Visitor<Object> {
+
+    private final Environment environment;
+
+    public AstCodeGen(Environment environment){
+        this.environment = environment;
+    }
+    public Object interpret(Exp exp){
+        return exp.accept(this);
+    }
+    @Override
+    public Object visitDyadExp(DyadExp expr) {
+        double res = 0;
+        switch (expr.op){
+            case SymOpExp s -> {
+
+            }
+            case OpExp s -> {
+                if ( s.name.compareTo("+") == 0) res = (double)expr.left.accept(this) + (double)expr.right.accept(this);
+            }
+            default -> throw new IllegalStateException("Unexpected value: " + expr.op);
+        }
+        return res;
+    }
+
+    @Override
+    public Object visitMonadExp(MonadExp expr) {
+        double res = 0;
+        switch (expr.op){
+            case SymOpExp s -> {
+                if (expr.exp == null) {
+                    res = (double)expr.op.accept(this);
+                }
+                else res = 2*(double)expr.exp.accept(this);
+            }
+            case OpExp s -> {
+                if ( s.name.compareTo("+") == 0) res = (double)expr.exp.accept(this);
+            }
+            default -> res = (double) expr.exp.accept(this);
+        }
+        return res;
+    }
+
+    @Override
+    public Object visitNounExp(NounExp expr) {
+        return expr.scalar ?
+                expr.single :
+                expr.val.get(0);
+    }
+
+    @Override
+    public Object visitAdverb(Adverb expr) {
+        return null;
+    }
+
+    @Override
+    public Object visitAssignExp(AssignExp expr) {
+        Object result = expr.exp.accept(this);
+        environment.assign(expr.name,result);
+        return result;
+    }
+
+    @Override
+    public Object visitEachExp(EachExp expr) {
+        return null;
+    }
+
+    @Override
+    public Object visitEachLeftExp(EachLeftExp expr) {
+        return null;
+    }
+
+    @Override
+    public Object visitEachPairExp(EachPairExp expr) {
+        return null;
+    }
+
+    @Override
+    public Object visitEachRightExp(EachRightExp expr) {
+        return null;
+    }
+
+    @Override
+    public Object visitFuncCallExp(FuncCallExp expr) {
+        return null;
+    }
+
+    @Override
+    public Object visitFuncExp(FuncExp expr) {
+        return null;
+    }
+
+    @Override
+    public Object visitListExp(ListExp expr) {
+        return null;
+    }
+
+    @Override
+    public Object visitOverExp(OverExp expr) {
+        return null;
+    }
+
+    @Override
+    public Object visitScanExp(ScanExp expr) {
+        return null;
+    }
+
+    @Override
+    public Object visitSymExp(SymExp expr) {
+        return null;
+    }
+
+    @Override
+    public Object visitSymOpExp(SymOpExp expr) {
+        return environment.get(expr.name);
+    }
+
+    @Override
+    public Object visitOpExp(OpExp expr) {
+        return null;
+    }
+
+
+}
---
+++ src/AstPrinter.java
@@ -0,0 +1,124 @@
+import java.util.List;
+
+public class AstPrinter implements Exp.Visitor<String> {
+
+    public String print(Exp exp){
+        return exp.accept(this);
+    }
+    @Override
+    public String visitDyadExp(DyadExp expr) {
+        return parens(expr.op.accept(this),expr.left,expr.right);
+    }
+
+    @Override
+    public String visitMonadExp(MonadExp expr) {
+        return parens(expr.op.accept(this),expr.exp);
+    }
+
+    @Override
+    public String visitNounExp(NounExp expr) {
+        return expr.scalar ?
+                parens("Obj",expr.single) :
+                parens("Obj", expr.val);
+    }
+
+    @Override
+    public String visitAdverb(Adverb expr) {
+        return null;
+    }
+
+    @Override
+    public String visitAssignExp(AssignExp expr) {
+        return parens(String.format("= %s",expr.name),  expr.exp);
+    }
+
+    @Override
+    public String visitEachExp(EachExp expr) {
+        return null;
+    }
+
+    @Override
+    public String visitEachLeftExp(EachLeftExp expr) {
+        return null;
+    }
+
+    @Override
+    public String visitEachPairExp(EachPairExp expr) {
+        return null;
+    }
+
+    @Override
+    public String visitEachRightExp(EachRightExp expr) {
+        return null;
+    }
+
+    @Override
+    public String visitFuncCallExp(FuncCallExp expr) {
+        return null;
+    }
+
+    @Override
+    public String visitFuncExp(FuncExp expr) {
+        return null;
+    }
+
+    @Override
+    public String visitListExp(ListExp expr) {
+        return null;
+    }
+
+    @Override
+    public String visitOverExp(OverExp expr) {
+        return null;
+    }
+
+    @Override
+    public String visitScanExp(ScanExp expr) {
+        return null;
+    }
+
+    @Override
+    public String visitSymExp(SymExp expr) {
+        System.out.println("sym");
+        return expr.name;
+    }
+
+    @Override
+    public String visitSymOpExp(SymOpExp expr) {
+        return expr.name;
+    }
+
+    @Override
+    public String visitOpExp(OpExp expr) {
+        return expr.name;
+    }
+
+
+    private String parens(String name, Exp... exps){
+        StringBuilder sb = new StringBuilder();
+        sb.append("(").append(name);
+        for (Exp exp : exps){
+            sb.append(" ");
+            if (exp != null) sb.append(exp.accept(this));
+            else sb.append("null");//sb.deleteCharAt(sb.lastIndexOf(" "));
+        }
+        sb.append(")");
+        return sb.toString();
+    }
+    private String parens(String name, List val){
+        StringBuilder sb = new StringBuilder();
+        sb.append("(").append(name);
+        sb.append(" ");
+        sb.append(val);
+        sb.append(")");
+        return sb.toString();
+    }
+    private String parens(String name, double val){
+        StringBuilder sb = new StringBuilder();
+        sb.append("(").append(name);
+        sb.append(" ");
+        sb.append(val);
+        sb.append(")");
+        return sb.toString();
+    }
+}
